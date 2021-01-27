4cc3b7ce81cd1df78e4e8dc8c0cb21a50fd06659
PPPrior <1256961901@qq.com>
2021-01-28 02:15:44 +0800

    2021.01.28

---
+++ 0605_Can_Place_Flowers.py
@@ -0,0 +1,15 @@
+""" Array """
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i - 1] == flowerbed[i] == flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        return n <= 0
+
+
+print(Solution().canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2))
---
+++ 0763_Partition_Labels.py
@@ -0,0 +1,18 @@
+""" Two Pointers """
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        d = {k: v for v, k in enumerate(S)}
+        ans = []
+        l = r = 0
+        for i, s in enumerate(S):
+            r = max(r, d[s])
+            if i == r:
+                ans.append(r - l + 1)
+                l = r + 1
+        return ans
+
+
+print(Solution().partitionLabels("ababcbacadefegdehijhklij"))
