6646977b648829fe4d4d2a2b22d41b36613995c5
PPPrior <1256961901@qq.com>
2021-03-16 15:43:57 +0800

    2021.03.15

--- 0347_Top_K_Frequent_Elements.py
+++ 0347_Top_K_Frequent_Elements.py
@@ -5,11 +5,16 @@ from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = defaultdict(int)
+        d, n, ans = defaultdict(int), len(nums), list()
         for num in nums:
             d[num] += 1
-        d = sorted(d.items(), key=lambda x: -x[1])
-        return [x[0] for x in d[:k]]
+        # Bucket Sort
+        buckets = [[] for _ in range(n + 1)]
+        for key, val in d.items():
+            buckets[val].append(key)
+        for bucket in buckets[::-1]:
+            ans += bucket if bucket else []
+        return ans[:k]
 
 
-print(Solution().topKFrequent(nums=[1, 1, 1, 2, 2, 2, 2, 3], k=2))
+print(Solution().topKFrequent(nums=[1, 2], k=2))
