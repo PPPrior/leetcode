09c8215a6d80a67c4c3048d49ca5c1f2ae9c0407
PPPrior <1256961901@qq.com>
2022-01-13 17:41:49 +0800

    2022.01.10

---
+++ 0946_Validate_Stack_Sequences.py
@@ -0,0 +1,16 @@
+""" Stack """
+from typing import List
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack, i = [], 0
+        for num in pushed:
+            stack.append(num)
+            while stack and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+        return not stack
+
+
+print(Solution().validateStackSequences(pushed=[1, 2, 3, 4, 5], popped=[4, 3, 5, 1, 2]))
